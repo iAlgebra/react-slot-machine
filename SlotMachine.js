71c822e04efb0aae59254a6a993cd3a076b70020
Maria Gabriela Barrios Cantera <barrios.cantera@gmail.com>
2020-01-30 16:12:37 +1100

    refactor

--- SlotMachine.js
+++ SlotMachine.js
@@ -2,7 +2,7 @@ class SlotMachine extends React.Component {
   render() {
     let msg;
     const {s1, s2, s3} = this.props;
-    if (s1 === s2 && s1 === s3 && s1 !== null) {
+    if (s1 === s2 && s1 === s3) {
       msg = 'You win!';
     } else {
       msg = 'You lose!';
